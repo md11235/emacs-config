7d6968d013ad0978cae946f01de44f84b9d9f751
Madsen <md11235@gmail.com>
2016-11-27 00:19:37 +0800

    configure ELPY for python

--- linux/python-mode-config.el
+++ linux/python-mode-config.el
@@ -1,1 +1,10 @@
+(require 'elpy)
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+
+(add-hook 'python-mode-hook '(lambda ()
+                               (elpy-mode t)
+                               (elpy-enable)
+                               (setq elpy-rpc-backend "jedi")))
+
+(eval-after-load 'elpy
+  '(define-key elpy-mode-map (kbd "C-c C-e") 'elpy-shell-send-current-statement))
