fada5020ff3c3f5dcc1249402ef59132e5d34c84
Madsen <md11235@gmail.com>
2016-11-26 09:37:02 +0800

    add more packages

---
+++ shared/elpa/rope-read-mode-20160603.9/rope-read-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rope-read-mode" "20160603.9" "Rearrange lines to read text smoothly" 'nil :url "https://github.com/marcowahl/rope-read-mode" :keywords '("reading" "convenience" "chill"))
