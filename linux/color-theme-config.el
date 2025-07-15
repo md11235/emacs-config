c6d2baafc5adbe83403fe7385a5c3e2463bec723
Sam <md11235@gmail.com>
2025-07-15 10:43:09 +0800

    启用color theme modern

--- linux/color-theme-config.el
+++ linux/color-theme-config.el
@@ -1,17 +1,26 @@
 ;;;;;color-theme
 ;; (load-library "color-theme")
-(require 'color-theme)
+(require 'color-theme-modern)
 
-(color-theme-initialize)
-(setq color-theme-is-global t)
+(add-to-list 'custom-theme-load-path
+             (file-name-as-directory
+              (expand-file-name "~/utility-config/emacs-config/replace-colorthemes")))
 
-;;(color-theme-marine)
-(require 'color-theme-billc)
+(load-theme 'billw t t)
+(enable-theme 'billw)
 
-(if (display-graphic-p) ;; window-system
-    (color-theme-feng-shui)
-  ;;(color-theme-renegade)
-  (color-theme-arjen))
+;; (if (display-graphic-p) ;; window-system
+;;     (color-theme-feng-shui)
+;;   ;;(color-theme-renegade)
+;;   (color-theme-arjen))
+
+;; (color-theme-initialize)
+;; (setq color-theme-is-global t)
+
+;; ;;(color-theme-marine)
+;; (require 'color-theme-billc)
+
+
 
 ;; (add-hook 'before-make-frame-hook
 ;;           (lambda (frame)
